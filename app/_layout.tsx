96f3b6a8e2341fb96f2e9edf043e8a5adae08169
TheLegend270 <moritz.joerg@gmx.de>
2025-12-11 20:39:38 +0700

    fix: remove Analytics to fix white screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -205,7 +205,6 @@ export default function RootLayout() {
         />
       </Stack>
       <StatusBar style="auto" />
-      {Platform.OS === 'web' && <Analytics />}
     </ThemeProvider>
   );
 }
